cafa88426c43c622a849199032015cbedc9d3422
Daxi90 <hdateam90@gmail.com>
2023-07-18 10:14:40 +0200

    Added CRUD functionality

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
-console.log("Geladen");
+let contacts;
+
+async function getContactData() {
+  let response = await fetch("./contacts.json");
+  contacts = await response.json();
+}
+
+async function init() {
+  await getContactData();
+  renderContacts();
+}
+
+function renderContacts() {
+  let container = document.getElementById("contactsContainer");
+  container.innerHTML = "";
+
+  for (let i = 0; i < contacts.length; i++) {
+    container.innerHTML += `
+            <div class="contactCard">
+                <span>${contacts[i].vorname} ${contacts[i].nachname}</span>
+                <span>${contacts[i].email}</span>
+                <span>${contacts[i].phone}</span>
+                <img onclick="deleteEntry(${i})" class="trash-bin" src="./img/delete.png" alt="">
+            </div>
+        `;
+  }
+}
+
+function deleteEntry(entry) {
+  contacts.splice(entry, 1);
+  renderContacts();
+}
+
+function addEntry() {
+  let vorname = document.getElementById("vorname").value;
+  let nachname = document.getElementById("nachname").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+
+  let newEntry = {
+    vorname: vorname,
+    nachname: nachname,
+    email: email,
+    phone: phone,
+  };
+
+  contacts.push(newEntry);
+  clearInput();
+  renderContacts();
+}
+
+function clearInput() {
+  let vorname = document.getElementById("vorname");
+  let nachname = document.getElementById("nachname");
+  let email = document.getElementById("email");
+  let phone = document.getElementById("phone");
+
+  vorname.value = "";
+  nachname.value = "";
+  email.value = "";
+  phone.value = "";
+}
